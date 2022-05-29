b51ad9b0c2b84dbd776d0e586207f4e3e9ccee76
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-29 14:50:11 -0300

    le meti color en ingles corregi error en linea 1 y 6

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
-let nArray: number = Number(prompt("ingrese la cantidad de nombres a cargar:"));
-let arrayName: string[] = new Array(nArray);
+let nArray: number = Number(prompt("insert the long of the array:"));
+let arrayName = new Array(nArray);
 let i: number;
 
 for (i = 0; i < nArray; i++) {
-  arrayName[i] = prompt(
-    "ingrese el nombre que desea colocar ena la posicion: " + i
-  );
+  arrayName[i] = prompt("enter the desired name in the position: " + i);
 }
 
 for (i = 0; i < nArray; i++) {
   console.log(
-    "el nommbre que usted ingreso en la posicion " + i,
-    "es: " + arrayName[i]
+    "the name you entered in the position " + i,
+    "is: " + arrayName[i]
   );
 }
